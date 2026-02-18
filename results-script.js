ff3c0a2bfce62c61bf8fb6dab52cb289ce8d19a4
Susieaa <62160477+Susieaa@users.noreply.github.com>
2026-02-18 20:04:03 +0100

    Update results-script.js

--- results-script.js
+++ results-script.js
@@ -1,15 +1,16 @@
 const wpm=localStorage.getItem("wpm");
 const accuracy=localStorage.getItem("accuracy");
 const wpmRecord=localStorage.getItem("wpmRecord");
+const score = Math.round((accuracy*wpm)/100);
 
 
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("WPM-results").textContent = wpm;
     document.getElementById("Accuracy").textContent = accuracy+"%";
-    document.getElementById("Score").textContent = Math.round((accuracy*wpm)/100);
+    document.getElementById("Score").textContent = score;
     document.getElementById("record-WPM").textContent = wpmRecord;
-    if(wpm>wpmRecord){
+    if(score>wpmRecord){
         document.getElementById("image-results").src = "assets/images/icon-new-pb.svg";
     }
 });
@@ -18,5 +19,4 @@ document.addEventListener('DOMContentLoaded', function() {
 function restartTest(){
     localStorage.setItem("firstPlay", false);
     location.replace("./index.html") 
-
 }
